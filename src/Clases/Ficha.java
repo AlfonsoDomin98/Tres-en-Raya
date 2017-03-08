e9bd076a7810d40190297519958043e3accf3a5d
Alfonso <Alfonso@192.168.1.196>
2017-03-08 12:08:34 +0100

    Tres en Raya

---
+++ src/Clases/Ficha.java
@@ -0,0 +1,22 @@
+package Clases;
+
+public class Ficha {
+	private String ficha;
+
+    public Ficha() {
+    }
+
+    public Ficha(String ficha) {
+        this.ficha = ficha;
+    }
+
+    public String getFicha() {
+        return ficha;
+    }
+
+    public void setFicha(String ficha) {
+        this.ficha = ficha;
+    }
+    
+}
+
